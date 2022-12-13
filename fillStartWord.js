b90c47959b108eb9935bff07b75317fec2c577b9
napat103 <naphat.watt@kmutt.ac.th>
2022-12-13 22:46:42 +0700

    Finish

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,16 +1,9 @@
 const { template } = require('@babel/core')
 
-function fillStartWorld1(startWord, word) {
-  return console.log(word == null||undefined? undefined:(word.slice(0, startWord.length)==startWord? word:startWord+word))
+function fillStartWorld(startWord, word) {
+  if (word === null || word === undefined) return undefined
+  if (startWord === word.slice(0, startWord.length)) return word
+  return startWord + word
 }
 
-fillStartWorld1('12','arm12')
-fillStartWorld1('12', '12arm')
-fillStartWorld1('world', 'startworld')
-fillStartWorld1('start', 'startWord')
-fillStartWorld1('start', 'start')
-fillStartWorld1('JS', 'beginning')
-fillStartWorld1('start', null)
-fillStartWorld1('start', undefined)
-
-module.exports = fillStartWord
+module.exports = fillStartWorld
